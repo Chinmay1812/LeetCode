295d18bcce8b3cd3f88fbe1e0607c2de5ac501f2
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-02-16 15:24:08 +0530

    Time: 20 ms (69.11%), Space: 11 MB (71.43%) - LeetHub

---
+++ 265-paint-house-ii/265-paint-house-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minCostII(vector<vector<int>>&v) 
+    {
+        int n=v.size();
+        int k=v[0].size();
+        int dp[n][k];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<k;j++)
+            {
+                dp[i][j]=INT_MAX;
+            }
+        }
+        
+        for(int i=0;i<k;i++)
+        {
+            dp[0][i]=v[0][i];
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<k;j++)
+            {
+                int val=INT_MAX;
+                
+                for(int l=0;l<k;l++)
+                {
+                    if(l==j)
+                    {
+                        continue;
+                    }
+                    val=min(val,dp[i-1][l]);
+                }
+                dp[i][j]=v[i][j]+val;
+            }   
+        }
+        int ans=INT_MAX;
+        for(int i=0;i<k;i++)
+        {
+            ans=min(dp[n-1][i],ans);
+        }
+        return ans;
+    }
+};
