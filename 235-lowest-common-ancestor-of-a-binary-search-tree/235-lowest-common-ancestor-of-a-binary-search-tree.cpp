9c7af0d893c396c45ec1c34c2226489245be7840
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-02-22 18:16:16 +0530

    Time: 32 ms (77.95%), Space: 23.4 MB (12.94%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,69 +10,25 @@
 
 class Solution {
 public:
-//     void paths(TreeNode* root,TreeNode* node,vector<pair<TreeNode*,int>> &path)
-//     {
-//         if(!root)
-//         {
-//             return;
-//         }
-//         if(root->val==node->val)
-//         {
-//             path.push_back({root,node->val});
-//             return;
-//         }
-//         path.push_back({root,root->val});
-//         if(root->left && root->left->val<node->val)
-//         {
-//             paths(root->right,node,path);
-//         }
-//         else if(root->right && root->right->val>node->val)
-//         {
-//             paths(root->left,node,path);
-//         }
-//     }
-    
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
-//     {
-//         vector<pair<TreeNode*,int>> path1,path2;
-//         if(!root)
-//         {
-//             return root;
-//         }
-//         paths(root,p,path1);paths(root,q,path2);
-      
-//         int x=0;
-//         while(x<path1.size() && x<path2.size() && path1[x].second==path2[x].second)
-//         {
-//             x++;
-//         }
-        
-//         return path1[x].first;
-//     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-      if(!root||root==p||root==q) 
+        if(!root)
         {
             return root;
         }
-        
-        TreeNode* left=lowestCommonAncestor(root->left,p,q);
-        TreeNode* right=lowestCommonAncestor(root->right,p,q);
-        
-        if(left && right)
+        if(p->val<root->val && q->val<root->val)
         {
-            return root;
+            return lowestCommonAncestor(root->left,p,q);
         }
-        else if(left && !right)
+        if(p->val>root->val && q->val>root->val)
         {
-            return left;
+            return lowestCommonAncestor(root->right,p,q);
         }
         else
         {
-            return right;
+            return root;
         }
         
         return root;
-         
     }
 };
