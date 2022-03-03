fecef80a31f4bd8a59d8c954c4da4d65e700f0d1
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-03-03 11:11:20 +0530

    Time: 22 ms (47.02%), Space: 13.3 MB (54.76%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,29 +4,36 @@ public:
     {
         int n=v.size();
         int m=v[0].size();
-      unordered_map<int,int> row,col;
+        int flag=1;
+        
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            if(v[i][0]==0) flag=0;
+            
+            for(int j=1;j<m;j++)
             {
                 if(v[i][j]==0)
                 {
-                    row[i]=1;
-                    col[j]=1;
+                    v[i][0]=0;
+                    v[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<n;i++)
+        
+        for(int i=n-1;i>=0;i--)
         {
-            for(int j=0;j<m;j++)
+            for(int j=m-1;j>=1;j--)
             {
-                if(row[i]||col[j])
+                if(v[i][0]==0||v[0][j]==0)
                 {
                     v[i][j]=0;
                 }
-                
             }
-           
+            if(flag==0)
+            {
+                v[i][0]=0;
+            }
+            
         }
         
     }
