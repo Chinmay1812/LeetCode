85620ba4f41d4d782387efeafd78fc9506d421a6
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-03-11 15:40:22 +0530

    Time: 534 ms (10.62%), Space: 132.7 MB (11.76%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>&v) 
     {
-    
+        int n=v.size();
       unordered_map<int,int> m;
         for(auto x:v)
         {
           if(x>0)  m[x]=1;
         }
     
-        for(int i=1;i<1e6;i++)
+        for(int i=1;i<=n+1;i++)
         {
             if(m[i]==0)
             {
