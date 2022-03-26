3a24b23dae099a747e09d4fac16c12f632286249
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-03-26 12:34:49 +0530

    Time: 51 ms (16.04%), Space: 14.5 MB (37.39%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    	vector<int> topoSort(int n, vector<int> *adj) 
+	{
+	    unordered_map<int,int> m;
+	    for(int i=0;i<n;i++)
+	    {
+	        for(auto x:adj[i])
+	        {
+	            m[x]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(m[i]==0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    vector<int> ans;
+	    
+	    while(!q.empty())
+	    {
+	     int x=q.front();
+	     q.pop();
+	     for(int y:adj[x])
+	     {
+	         m[y]--;
+	         if(m[y]==0)
+	         {
+	             q.push(y);
+	         }
+	     }
+	        ans.push_back(x);
+	        
+	    }
+	    
+	    
+	    return ans;
+	    
+	}   
+    vector<int> findOrder(int n, vector<vector<int>>&graph) {
+        vector<int> v[n];
+        
+        for(auto x:graph)
+        {
+            v[x[1]].push_back(x[0]);
+        }
+        vector<int> temp=topoSort(n,v);
+        if(temp.size()==n)
+        {
+            return temp;
+        }
+        
+        temp.clear();
+        return temp;
+    }
+};
