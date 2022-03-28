f5067528423e1672797a0a676ccdf976d33c121a
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-03-28 14:55:45 +0530

    Time: 3 ms (95.97%), Space: 14.1 MB (32.08%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -2,14 +2,52 @@ class Solution {
 public:
     bool search(vector<int>&v, int k) 
     {
-        for(int x:v)
+        int n=v.size();
+        int s=0,e=n-1;
+        int ans=v[0];
+        while(s<=e)
         {
-            if(x==k)
+            while(s<e && v[s]==v[s+1])
             {
-                return 1;
+                s++;
             }
-        }
+            while(s<e && v[e]==v[e-1])
+            {
+                e--;
+            }
+            int mid=s+(e-s)/2;
+            if(v[mid]==k)
+            {
+                return true;
+            }
+            if(v[mid]>=v[s])
+            {
+                if(k<v[mid] && k>=v[s])
+                {
+                    e=mid-1;
+                }
+                else
+                {
+                    s=mid+1;
+                }
+            }
+            else
+            {
+                if(k<=v[e] && k>v[mid])
+                {
+                    s=mid+1;
+                }
+                else
+                {
+                    e=mid-1;
+                }
+            }
             
-        return 0;
+            
+        }
+        
+        
+        return false;
+        
     }
 };
