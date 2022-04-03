6e931585959ac1c13ea2ea5e70d0cf587af96f77
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-04-03 20:22:09 +0530

    Time: 18 ms (5.30%), Space: 9 MB (70.14%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
@@ -4,30 +4,18 @@ public:
     int twoSumLessThanK(vector<int>&b, int k) 
     {
        int n=b.size();
-        vector<int> v;
+        int ans=-1;
        for(int i=0;i+1<n;i++)
        {
            for(int j=i+1;j<n;j++)
            {
-               v.push_back(b[i]+b[j]);
+               if(b[i]+b[j]<k && ans<b[i]+b[j])
+               {
+                   ans=b[i]+b[j];
+               }
+             
            }
        }
-      sort(v.begin(),v.end());
-      int lo=0,hi=v.size()-1;
-        int ans=-1;
-        while(lo<=hi)
-        {
-            int mid=lo+(hi-lo)/2;
-            if(v[mid]<k)
-            {
-                ans=v[mid];
-                lo=mid+1;
-            }
-            else
-            {
-                hi=mid-1;
-            }
-        }
-        return ans;
+  return ans;
     }
 };
