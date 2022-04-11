e4ea6351cab8277034a5b6306e645dea4bf845cd
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-04-12 03:25:53 +0530

    Time: 344 ms (10.27%), Space: 9.2 MB (97.90%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,25 +1,21 @@
-class Solution
-{
-    public:
-        string removeDuplicates(string s, int k)
+class Solution {
+public:
+    string removeDuplicates(string s, int k)
+    {
+        stack<int> st;
+        for(int i=0;i<s.size();i++)
         {
-            vector<int> count(s.size());
-            for (int i = 0; i < s.size(); ++i)
+            if(i==0||s[i]!=s[i-1])
             {
-                if (i == 0 || s[i] != s[i - 1])
-                {
-                    count[i] = 1;
-                }
-                else
-                {
-                    count[i] = count[i - 1] + 1;
-                    if (count[i] == k)
-                    {
-                        s.erase(i - k + 1, k);
-                        i = i - k;
-                    }
-                }
+                st.push(1);
             }
-            return s;
+            else if(++st.top()==k)
+            {
+                st.pop();
+                s.erase(i-k+1,k);
+                i=i-k;
+            }
         }
+        return s;
+    }
 };
