1c3033f79ee3262be1ad0c927b748bb00d3f6905
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-04-18 23:49:51 +0530

    Time: 3 ms (95.33%), Space: 10 MB (79.48%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>&v1, vector<int>&v2) 
+    {
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        int i=0,j=0;
+        int n1=v1.size(),n2=v2.size();
+        vector<int> ans;
+        while(i<n1 && j<n2)
+        {
+            if(v1[i]>v2[j])
+            {
+                while(j<n2 && v1[i]>v2[j])
+                {
+                    j++;
+                }
+            }
+            else if(v1[i]<v2[j])
+            {
+                while(i<n1 && v1[i]<v2[j])
+                {
+                    i++;
+                }
+            }
+            else
+            {
+                ans.push_back(v1[i]);
+                i++;
+                j++;
+                while(i<n1 && v1[i]==v1[i-1])
+                {
+                    i++;
+                }
+                while(j<n2 && v2[j]==v2[j-1])
+                {
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+};
