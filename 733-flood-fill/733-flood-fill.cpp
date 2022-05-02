e3ebb141912e7a527e04ecc56e28906bb536caec
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-02 23:38:13 +0530

    Time: 31 ms (5.68%), Space: 14.5 MB (12.19%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int> > dir{{0,1},{0,-1},{1,0},{-1,0}};
+    
+    vector<vector<int>> floodFill(vector<vector<int>>&v, int sr, int sc, int k) 
+    {
+        vector<pair<int,int>> index;
+        int n=v.size();
+        int m=v[0].size();
+        int vis[n][m];
+        memset(vis,0,sizeof(vis));
+        queue<pair<int,int> > q;
+        q.push({sr,sc});
+        int val=v[sr][sc];
+        
+        index.push_back({sr,sc});
+        while(!q.empty())
+        {
+            auto var=q.front();
+            q.pop();
+            int x=var.first;
+            int y=var.second;
+            vis[x][y]=1;
+            for(int i=0;i<dir.size();i++)
+            {
+                int dx=x+dir[i][0];
+                int dy=y+dir[i][1];
+                if(dx>=n||dx<0||dy>=m||dy<0)
+                {
+                    continue;
+                }
+                if(!vis[dx][dy] && v[dx][dy]==val)
+                {
+                    index.push_back({dx,dy});
+                    q.push({dx,dy});
+                }
+            }
+        }
+        for(auto x:index)
+        {
+            v[x.first][x.second]=k;
+        }
+        
+        return v;
+    }
+};
