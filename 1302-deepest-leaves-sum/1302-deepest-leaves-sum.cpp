c560539c842ec99c916c3d30d7eb3f6ee16a7f1b
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-15 10:25:15 +0530

    Time: 123 ms (60.83%), Space: 61.9 MB (27.92%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,33 +11,34 @@
  */
 class Solution {
 public:
-    int ht(TreeNode* root)
+    int deepestLeavesSum(TreeNode* root)
     {
         if(!root)
         {
             return 0;
         }
-        return 1+max(ht(root->left),ht(root->right));
-    }
-    int sum=0;
-     void dfs(TreeNode* root,int h,int lev=1)
-     {
-         if(!root)
-         {
-             return;
-         }
-         if(h==lev)
-         {
-             sum+=root->val;
-         }
-         dfs(root->left,h,lev+1);
-         dfs(root->right,h,lev+1);
-     }
-    
-    int deepestLeavesSum(TreeNode* root) 
-    {
-        int h=ht(root);
-        dfs(root,h);
-        return sum;
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans=0;
+        while(!q.empty())
+        {
+            int i=0;
+            int n=q.size();
+            for(i=0,ans=0;i<n;i++)
+            {
+                auto node=q.front();
+                q.pop();
+                ans+=node->val;
+                if(node->left)
+                {
+                    q.push(node->left);
+                }
+                if(node->right)
+                {
+                    q.push(node->right);
+                }
+            }
+        }
+        return ans;
     }
 };
