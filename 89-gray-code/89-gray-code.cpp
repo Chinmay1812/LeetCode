9e623f6fffd77a484c1b6f6205e78a1c14fae13a
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-20 12:14:19 +0530

    Time: 87 ms (7.45%), Space: 29.5 MB (11.69%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -4,25 +4,44 @@ class Solution
 
         void fun(int n, vector<string> &v)
         {
-            if (n == 1)
+            // if (n == 1)
+            // {
+            //     v.push_back("0");
+            //     v.push_back("1");
+            //     return;
+            // }
+            // fun(n - 1, v);
+            // int x = v.size();
+            // for (int i = 0; i < x; i++)
+            // {
+            //     v.push_back(v[x - i - 1]);
+            // }
+            // for (int i = 0; i < x; i++)
+            // {
+            //     v[i] = "0" + v[i];
+            // }
+            // for (int i = v.size() - 1; i >= x; i--)
+            // {
+            //     v[i] = "1" + v[i];
+            // }
+            v.push_back("0");
+            v.push_back("1");
+            
+            for(int i=2;i<=n;i++)
             {
-                v.push_back("0");
-                v.push_back("1");
-                return;
-            }
-            fun(n - 1, v);
-            int x = v.size();
-            for (int i = 0; i < x; i++)
-            {
-                v.push_back(v[x - i - 1]);
-            }
-            for (int i = 0; i < x; i++)
-            {
-                v[i] = "0" + v[i];
-            }
-            for (int i = v.size() - 1; i >= x; i--)
-            {
-                v[i] = "1" + v[i];
+                int x=v.size();
+                for(int j=0;j<x;j++)
+                {
+                    v.push_back(v[x-1-j]);
+                }
+                for(int j=0;j<x;j++)
+                {
+                    v[j]="0"+v[j];
+                }
+                for(int j=v.size()-1;j>=x;j--)
+                {
+                    v[j]="1"+v[j];
+                }
             }
         }
     int toint(string s)
