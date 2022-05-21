767d7508301aec369a7efa87f6fb3cf52bcd39de
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-21 15:53:51 +0530

    Time: 7 ms (96.57%), Space: 6.9 MB (96.38%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    
-    int fun(int k, vector<int>&v,int i,int dp[][5001])
-    {
-        if(i>=v.size())
-        {
-            return 0;
-        }
-        if(k==0)
-        {
-            return 1;
-        }
-        if(k<0)
-        {
-            return 0;
-        }
-        if(dp[i][k]!=-1)
-        {
-            return dp[i][k];
-        }
-        
-       return dp[i][k] =fun(k-v[i],v,i,dp)+fun(k,v,i+1,dp);
-    }
-    
-    
     int change(int k, vector<int>&v) 
     {
-        int n=v.size();
-          int dp[301][5000+1];
-        memset(dp,-1,sizeof(dp));
-        return fun(k,v,0,dp);
-        
+        sort(v.begin(),v.end());
+        int dp[k+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        for(int &x:v)
+        {
+            
+            for(int i=1;i<=k;i++)
+            {
+                if(i-x>=0)
+                {
+                    dp[i]=dp[i]+dp[i-x];
+                }
+               
+            }
+            
+        }
+
+        return dp[k];
     }
 };
