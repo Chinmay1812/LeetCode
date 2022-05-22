61170e6e518e10657a75a62c7ef0e6df37f543e6
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-22 15:49:43 +0530

    Time: 9 ms (63.71%), Space: 7.3 MB (60.58%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    bool pal(string s)
-    {
-        int n=s.size();
-        for(int i=0;i<n/2;i++)
-        {
-            if(s[i]!=s[n-1-i])
-            {
-                return 0;
-            }
-        }
-        return 1;
-    }
-    
-    
     int countSubstrings(string s) 
     {
         int n=s.size();
+        bool dp[n+1][n+1];
+        memset(dp,0,sizeof(dp));
         int ans=0;
         for(int i=0;i<n;i++)
         {
-            string temp="";
-            temp+=s[i];
-            for(int j=i+1;j<n;j++)
-            {
-                temp+=s[j];
-                ans+=pal(temp);
-            }
+            dp[i][i]=1;
         }
         ans+=n;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]==s[i+1])
+            {
+                dp[i][i+1]=1;
+                ans++;
+            }
+        }
+        
+        
+        for(int k=2;k<n;k++)    // k here represents length of palindrome.
+        {
+            int j=k;
+            for(int i=0;i<n && j<n;i++,j++)
+            {
+                if(s[i]==s[j] && dp[i+1][j-1])
+                {
+                    dp[i][j]=1;
+                    ans++;
+                }
+            }
+        }
         return ans;
     }
 };
