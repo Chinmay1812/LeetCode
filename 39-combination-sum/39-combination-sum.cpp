5b116970f3daaa4582ed3b084e2cff8f81b67d63
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-23 14:56:06 +0530

    Time: 24 ms (37.93%), Space: 10.9 MB (59.37%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,34 +2,70 @@ class Solution {
 public:
     
     
-    void fun(vector<int>&v,int k,vector<int> &temp,vector<vector<int>> &ans,int i=0)
-    {
-        if(k==0)
-        {
-            ans.push_back(temp);
-            return;
-        }
-        if(k<0)
-        {
-            return;
-        }
+//     void fun(vector<int>&v,int k,vector<int> &temp,vector<vector<int>> &ans,int i=0)
+//     {
+//         if(k==0)
+//         {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         if(k<0)
+//         {
+//             return;
+//         }
         
-        for(int j=i;j<v.size();j++)
-        {
-            temp.push_back(v[j]);
-         if(k-v[j]>=0)   fun(v,k-v[j],temp,ans,j);
-            temp.pop_back();
-        }
+//         for(int j=i;j<v.size();j++)
+//         {
+//             temp.push_back(v[j]);
+//          if(k-v[j]>=0)   fun(v,k-v[j],temp,ans,j);
+//             temp.pop_back();
+//         }
+//     }
+    
+    
+    
+//     vector<vector<int>> combinationSum(vector<int>&v, int k)
+//     {
+//         int n=v.size();
+//         vector<int> temp;
+//         vector<vector<int>> ans;
+//         fun(v,k,temp,ans);
+//         return ans;
+        
+//     }
+    
+    vector<vector<int> > ans;
+    vector<int> temp;
+    set<vector<int> > s;
+    void fun(vector<int>&v,int k,int sum=0,int id=0)
+    {
+          if(sum>k)
+          {
+              return;
+          }
+          if(sum==k)
+          {
+              
+           
+             ans.push_back(temp);
+     
+             
+              return;
+          }
+      
+      for(int i=id;i<v.size();i++)
+      {
+          temp.push_back(v[i]);
+          fun(v,k,sum+v[i],i);        
+          temp.pop_back();
+      }
+    
     }
     
-    
-    
-    vector<vector<int>> combinationSum(vector<int>&v, int k)
+    vector<vector<int> > combinationSum(vector<int> &v, int k) 
     {
-        int n=v.size();
-        vector<int> temp;
-        vector<vector<int>> ans;
-        fun(v,k,temp,ans);
+        sort(v.begin(),v.end());
+        fun(v,k);
         return ans;
         
     }
