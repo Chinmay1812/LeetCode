862e01ddc905a6bd82f41dd179ccbb545e90f43d
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-23 14:27:48 +0530

    Time: 3 ms (68.11%), Space: 7.2 MB (68.51%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    
     vector<vector<int>> ans;
-    void fun(vector<int>&v,int n)
+    vector<int> temp;
+    void fun(vector<int>&v,int id)
     {
-         
-          
-          while(n)
-          {
-              int x=n;
-              int j=0;
-               vector<int> temp;
-              while(x)
-              {
-                  if(x&1)
-                  {
-                      temp.push_back(v[j]);
-                  }
-                  j++;
-                  x>>=1;
-              }
-              n--;
-              ans.push_back(temp);
-          }
-        
+      if(id==v.size())
+      {
+          ans.push_back(temp);
+          return;
+      }
+
+           temp.push_back(v[id]);
+           fun(v,id+1);
+           temp.pop_back();
+           fun(v,id+1);
+      
     }
-    
     
     vector<vector<int>> subsets(vector<int>&v) 
     {
-      
-          int n=pow(2,v.size());
-        fun(v,n-1);
-        ans.push_back(vector<int>{});
-        return ans;
+           fun(v,0);
+           return ans;
     }
 };
