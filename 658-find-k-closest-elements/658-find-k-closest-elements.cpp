256f1cd5e7008b2ded4fb0b3c3bd3b7ebe3bbfef
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-26 01:44:13 +0530

    Time: 103 ms (31.83%), Space: 31.7 MB (67.55%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,45 +1,12 @@
 class Solution {
 public:
-    class two
-    {
-        public:
-        int val;
-        int num;
-        
-    };
-    
-    class cmp
-    {
-        public:
-        bool operator()(two &a,two &b)
-        {
-            if(a.val==b.val)
-            {
-                return a.num>b.num;
-            }
-            return a.val>b.val;
-        }
-    };
-    
-    
     vector<int> findClosestElements(vector<int>&v, int k, int x) 
     {
-        int n=v.size();
-        priority_queue<two,vector<two>,cmp> pq;
-        for(auto &z:v)
-        {
-            two var;
-            var.val=abs(z-x);
-            var.num=z;
-            pq.push(var);
-        }
-        vector<int> ans;
-        for(int i=0;i<k;i++)
-        {
-            ans.push_back(pq.top().num);
-            pq.pop();
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+      stable_sort(v.begin(),v.end(),[x](int a,int b){
+          return abs(a-x)<abs(b-x);
+      });
+      v.resize(k);
+      sort(v.begin(),v.end());
+      return v;  
     }
 };
