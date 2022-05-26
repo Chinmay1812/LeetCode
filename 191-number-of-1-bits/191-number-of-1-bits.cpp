30888ccc6393299b72b3b568ec2e7f98822dbeea
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-05-26 12:30:05 +0530

    Time: 3 ms (54.92%), Space: 5.9 MB (79.17%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -5,8 +5,8 @@ public:
          int ans=0;
         while(n)
         {
-            ans+=(n&1);
-            n>>=1;
+            ans++;
+            n=n&(n-1);
         }
         return ans;
     }
