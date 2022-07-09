e3430719a12d630ff7ca73db21103c6cf4503587
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-07-10 01:30:09 +0530

    Time: 166 ms (75.19%), Space: 5.8 MB (99.76%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSquares(int n) 
+    {
+        int dp[n+1];
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]=i;
+        }
+        dp[0]=0;
+        for(int i=1;i*i<=n;i++)
+            dp[i*i]=1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j*j<=i;j++)
+            {
+                dp[i]=min(dp[i],1+dp[i-j*j]);    
+            }
+        }
+        return dp[n];
+    }
+};
