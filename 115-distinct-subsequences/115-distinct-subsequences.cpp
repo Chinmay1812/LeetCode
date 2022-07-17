b6bd79469f225832c88a7b9af0ff9e1a3d8b8095
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-07-17 18:29:29 +0530

    Time: 33 ms (86.40%), Space: 6.5 MB (89.26%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -5,24 +5,18 @@ public:
     {
        int n=s.size();
        int m=g.size();
-      unsigned __int128 dp[n+1][m+1];
-       memset(dp,0,sizeof(dp));
-       for(int i=0;i<=n;i++) dp[i][0]=1;
-        
+       vector<double> temp(m+1,0);
+       temp[0]=1;  
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=m;j++)
+            for(int j=m;j>=1;j--)
             {
                 if(s[i-1]==g[j-1])
                 {
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j];
+                    temp[j]+=temp[j-1];
                 }
             }
         }
-        return dp[n][m];
+        return (int)temp[m];
     }
 };
