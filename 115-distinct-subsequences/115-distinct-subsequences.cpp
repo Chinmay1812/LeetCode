6077d079d347181ab7b4b03e7821c09798a8ecf9
Chinmay Jain <86679550+Chinmay1812@users.noreply.github.com>
2022-07-17 18:08:04 +0530

    Time: 37 ms (82.02%), Space: 24 MB (34.10%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int fun(string &s,string &g,int i,int j)
-    {
-       if(j<0) return 1;
-       if(i<0) return 0;
-       
-       if(dp[i][j]!=-1) 
-       {
-           return dp[i][j];
-       }
-       
-       if(s[i]==g[j])
-       {
-           dp[i][j]=fun(s,g,i-1,j-1)+fun(s,g,i-1,j);
-       }
-       else
-       {
-           dp[i][j]=fun(s,g,i-1,j);
-       }
-       return dp[i][j]; 
-    }
-    
+ 
     int numDistinct(string s, string g) 
     {
-       memset(dp,-1,sizeof(dp));
-       return fun(s,g,s.size()-1,g.size()-1);   
+       int n=s.size();
+       int m=g.size();
+      unsigned __int128 dp[n+1][m+1];
+       memset(dp,0,sizeof(dp));
+       for(int i=0;i<=n;i++) dp[i][0]=1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==g[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
